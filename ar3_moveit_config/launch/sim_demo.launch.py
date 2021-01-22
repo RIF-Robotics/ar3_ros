5602e70dd478d8ed80af068913c85b0089b3bf12
Kevin DeMarco <kevin.demarco@gmail.com>
2021-01-22 13:37:47 -0500

    mostly working moveit integration

--- ar3_moveit_config/launch/sim_demo.launch.py
+++ ar3_moveit_config/launch/sim_demo.launch.py
@@ -5,19 +5,19 @@ from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 from launch.substitutions import LaunchConfiguration, Command
 
-def load_file(package_name, file_path):
-    package_path = get_package_share_directory(package_name)
-    absolute_file_path = os.path.join(package_path, file_path)
+#def load_file(package_name, file_path):
+#    package_path = get_package_share_directory(package_name)
+#    absolute_file_path = os.path.join(package_path, file_path)
+#
+#    try:
+#        with open(absolute_file_path, 'r') as file:
+#            return file.read()
+#    except EnvironmentError: # parent of IOError, OSError *and* WindowsError where available
+#        return None
 
-    try:
-        with open(absolute_file_path, 'r') as file:
-            return file.read()
-    except EnvironmentError: # parent of IOError, OSError *and* WindowsError where available
-        return None
-
-def load_yaml(package_name, file_path):
+def load_yaml(package_name, *paths):
     package_path = get_package_share_directory(package_name)
-    absolute_file_path = os.path.join(package_path, file_path)
+    absolute_file_path = os.path.join(package_path, *paths)
 
     try:
         with open(absolute_file_path, 'r') as file:
@@ -25,56 +25,71 @@ def load_yaml(package_name, file_path):
     except EnvironmentError: # parent of IOError, OSError *and* WindowsError where available
         return None
 
+#def load_yaml(package_name, file_path):
+#    package_path = get_package_share_directory(package_name)
+#    absolute_file_path = os.path.join(package_path, file_path)
+#
+#    try:
+#        with open(absolute_file_path, 'r') as file:
+#            return yaml.safe_load(file)
+#    except EnvironmentError: # parent of IOError, OSError *and* WindowsError where available
+#        return None
+
 
 def generate_launch_description():
-    use_sim_time = LaunchConfiguration('use_sim_time', default='false')
+    #use_sim_time = LaunchConfiguration('use_sim_time', default='false')
 
-    xacro_path = os.path.join(get_package_share_directory('ar3_description'),
-                              'urdf', 'ar3.urdf.xacro')
-    robot_description = {'robot_description' : Command(['xacro', ' ', xacro_path])}
+    # Find and parse URDF xacro
+    urdf_xacro_path = os.path.join(get_package_share_directory('ar3_description'),
+                                   'urdf', 'ar3.urdf.xacro')
+    robot_description = {'robot_description' : Command(['xacro', ' ', urdf_xacro_path])}
 
-    #robot_description_semantic_config = load_file('ar3_moveit_config', 'config/ar3.srdf')
-    #robot_description_semantic = {'robot_description_semantic' : robot_description_semantic_config}
+    # Find and parse SRDF xacro
+    srdf_xacro_path = os.path.join(get_package_share_directory('ar3_moveit_config'),
+                                   'config', 'ar3_arm.srdf.xacro')
 
-    #kinematics_yaml = load_yaml('ar3_moveit_config', 'config/kinematics.yaml')
-    #robot_description_kinematics = { 'robot_description_kinematics' : kinematics_yaml }
+    robot_description_semantic = {'robot_description_semantic' :
+                                  Command(['xacro', ' ', srdf_xacro_path])}
 
-    ## Planning Functionality
-    #ompl_planning_pipeline_config = { 'move_group' : {
-    #    'planning_plugin' : 'ompl_interface/OMPLPlanner',
-    #    'request_adapters' : """default_planner_request_adapters/AddTimeOptimalParameterization default_planner_request_adapters/FixWorkspaceBounds default_planner_request_adapters/FixStartStateBounds default_planner_request_adapters/FixStartStateCollision default_planner_request_adapters/FixStartStatePathConstraints""" ,
-    #    'start_state_max_bounds_error' : 0.1 } }
-    #ompl_planning_yaml = load_yaml('ar3_moveit_config', 'config/ompl_planning.yaml')
-    #ompl_planning_pipeline_config['move_group'].update(ompl_planning_yaml)
+    kinematics_yaml = load_yaml('ar3_moveit_config', 'config', 'kinematics.yaml')
+    robot_description_kinematics = { 'robot_description_kinematics' : kinematics_yaml }
 
-    #controllers_yaml = load_yaml(os.path.join('ar3_moveit_config', 'config',
-    #                                          'fake_controllers.yaml'))
+    # Planning Functionality
+    ompl_planning_pipeline_config = { 'move_group' : {
+        'planning_plugin' : 'ompl_interface/OMPLPlanner',
+        'request_adapters' : """default_planner_request_adapters/AddTimeOptimalParameterization default_planner_request_adapters/FixWorkspaceBounds default_planner_request_adapters/FixStartStateBounds default_planner_request_adapters/FixStartStateCollision default_planner_request_adapters/FixStartStatePathConstraints""" ,
+        'start_state_max_bounds_error' : 0.1 } }
+    ompl_planning_yaml = load_yaml('ar3_moveit_config', 'config', 'ompl_planning.yaml')
+    ompl_planning_pipeline_config['move_group'].update(ompl_planning_yaml)
 
-    #moveit_controllers = { 'moveit_simple_controller_manager' : controllers_yaml,
-    #                       'moveit_controller_manager': 'moveit_simple_controller_manager/MoveItSimpleControllerManager'}
+    controllers_yaml = load_yaml('ar3_moveit_config', 'config',
+                                 'fake_controllers.yaml')
 
-    #trajectory_execution = {'moveit_manage_controllers': True,
-    #                        'trajectory_execution.allowed_execution_duration_scaling': 1.2,
-    #                        'trajectory_execution.allowed_goal_duration_margin': 0.5,
-    #                        'trajectory_execution.allowed_start_tolerance': 0.01}
-    #
-    #planning_scene_monitor_parameters = {"publish_planning_scene": True,
-    #             "publish_geometry_updates": True,
-    #             "publish_state_updates": True,
-    #             "publish_transforms_updates": True}
-    #
-    ## Start the actual move_group node/action server
-    #run_move_group_node = Node(package='moveit_ros_move_group',
-    #                           executable='move_group',
-    #                           output='screen',
-    #                           emulate_tty=True,
-    #                           parameters=[robot_description,
-    #                                       robot_description_semantic,
-    #                                       kinematics_yaml,
-    #                                       ompl_planning_pipeline_config,
-    #                                       trajectory_execution,
-    #                                       moveit_controllers,
-    #                                       planning_scene_monitor_parameters])
+    moveit_controllers = { 'moveit_simple_controller_manager' : controllers_yaml,
+                           'moveit_controller_manager': 'moveit_simple_controller_manager/MoveItSimpleControllerManager'}
+
+    trajectory_execution = {'moveit_manage_controllers': True,
+                            'trajectory_execution.allowed_execution_duration_scaling': 1.2,
+                            'trajectory_execution.allowed_goal_duration_margin': 0.5,
+                            'trajectory_execution.allowed_start_tolerance': 0.01}
+
+    planning_scene_monitor_parameters = {"publish_planning_scene": True,
+                                         "publish_geometry_updates": True,
+                                         "publish_state_updates": True,
+                                         "publish_transforms_updates": True}
+
+    # Start the actual move_group node/action server
+    run_move_group_node = Node(package='moveit_ros_move_group',
+                               executable='move_group',
+                               output='screen',
+                               emulate_tty=True,
+                               parameters=[robot_description,
+                                           robot_description_semantic,
+                                           kinematics_yaml,
+                                           ompl_planning_pipeline_config,
+                                           trajectory_execution,
+                                           moveit_controllers,
+                                           planning_scene_monitor_parameters])
 
     # RViz
     rviz_config_file = os.path.join(get_package_share_directory('ar3_moveit_config'), 'rviz', 'demo.rviz')
@@ -83,10 +98,10 @@ def generate_launch_description():
                      name='rviz2',
                      output='log',
                      arguments=['-d', rviz_config_file],
-                     parameters=[robot_description])
-                                 #robot_description_semantic,
-                                 #ompl_planning_pipeline_config,
-                                 #kinematics_yaml])
+                     parameters=[robot_description,
+                                 robot_description_semantic,
+                                 ompl_planning_pipeline_config,
+                                 kinematics_yaml])
 
     # Static TF
     static_tf = Node(package='tf2_ros',
@@ -112,4 +127,5 @@ def generate_launch_description():
                                               os.path.join(get_package_share_directory("ar3_moveit_config"), "config", "ar3_controllers.yaml"),
                                               robot_description])
 
-    return LaunchDescription([ rviz_node, static_tf, robot_state_publisher, fake_joint_driver_node ])
+    return LaunchDescription([
+        rviz_node, static_tf, robot_state_publisher, run_move_group_node, fake_joint_driver_node])
