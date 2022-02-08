5f6b482f311ff97ee761a63659a981aed1e91804
Kevin DeMarco <kevin@kevindemarco.com>
2022-02-08 17:19:01 -0500

    first working control of AR3 robot arm with moveit2

--- ar3_bringup/launch/ar3_base.launch.py
+++ ar3_bringup/launch/ar3_base.launch.py
@@ -94,7 +94,8 @@ def generate_launch_description():
     declared_arguments.append(
         DeclareLaunchArgument(
             "robot_controller",
-            default_value="forward_position_controller",
+            #default_value="forward_position_controller",
+            default_value="joint_trajectory_controller",
             description="Robot controller to start.",
         )
     )
